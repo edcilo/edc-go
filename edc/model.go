b138a2acfcfbe6215001a7ed30cb5df4c659ec74
edcilo <edcilop@gmail.com>
2024-03-29 10:06:45 -0600

    chore(base model): adding uuid generation before to create a new registry in db

--- edc/model.go
+++ edc/model.go
@@ -8,8 +8,14 @@ import (
 )
 
 type BaseModel struct {
-	ID        uuid.UUID      `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id" redis:"id"`
+	ID        uuid.UUID      `gorm:"type:uuid;primary_key" json:"id" redis:"id"`
 	CreatedAt time.Time      `gorm:"column:created_at" json:"created_at" redis:"created_at"`
 	UpdatedAt time.Time      `gorm:"column:updated_at" json:"updated_at" redis:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at;index" json:"-" redis:"-"`
 }
+
+func (m *BaseModel) BeforeCreate(tx *gorm.DB) error {
+  m.ID = uuid.New()
+  return nil
+}
+
