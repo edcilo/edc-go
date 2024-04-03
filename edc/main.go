825e1b79d8ab0e0ce26f16ff7fab1a33e3cd2118
edcilo <edcilop@gmail.com>
2024-04-02 20:03:17 -0600

    chore(http): change attribute name from App to Http

--- edc/main.go
+++ edc/main.go
@@ -13,13 +13,13 @@ type EDC struct {
 	Config *Configuration
 	DB     *gorm.DB
 	Cache  *redis.Client
-	App    *fiber.App
+	Http   *fiber.App
 }
 
 func (edc *EDC) Serve() {
 	log.Info("Starting EDC server")
 
-	if error := edc.App.Listen(fmt.Sprintf(
+	if error := edc.Http.Listen(fmt.Sprintf(
 		"%s:%d",
 		edc.Config.App.Host,
 		edc.Config.App.Port,
@@ -37,7 +37,7 @@ func Initialize(envfiles ...string) *EDC {
 		Config: ConfigSetup(envfiles...),
 		DB:     DBSetup(),
 		Cache:  CacheSetup(),
-		App:    ServerSetup(),
+		Http:   ServerSetup(),
 	}
 
 	return &Edc
